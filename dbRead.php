519c0d7451163d39a1cc0a37836599608b79616d
Sumaiya Gawhar Rafia <sumaiyarafia232@gmail.com>
2021-07-28 14:11:27 +0600

    1st commit

--- dbRead.php
+++ dbRead.php
@@ -7,7 +7,27 @@
   $sql->bind_param("ss",$userName,$password);
 $sql->execute();
 res= $sql->get_result();
-  return $sql->num_rows===1;
+  return $res->num_rows===1;
 
 }
+
+
+function getAllUsers(){
+  $conn = connect();
+  $sql= $conn->prepare("SELECT id, username FROM USERS");
+  $sql->execute();
+  res= $sql->get_result();
+  return $res->fetch_all(MYSQLI_ASSOC);
+}
+
+function getUser($username){
+  $conn = connect();
+  $sql= $conn->prepare("SELECT id, username FROM USERS WHERE username= ?");
+  $sql->bind_param("s",$userName);
+  $sql->execute();
+  res= $sql->get_result();
+  return $res->fetch_all(MYSQLI_ASSOC);
+}
+
+
 ?>
